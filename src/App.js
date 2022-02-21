e2a5c1caa200b79f829182fa578290a8bc941a49
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-21 15:45:37 -0800

    created private route and auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Home from './views/Home/Home';
 import './App.css';
 import Header from './components/Header/Header';
+import { useTheme } from './context/ThemeContext';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, Switch } from 'react-router-dom';
+import Auth from './views/Auth/Auth';
 
 function App() {
+  const { theme } = useTheme();
   return (
-    <div className="App">
-      {/* const {setUser} = useUser(); */}
-      <Header />
-      <Home />
+    <div className="App" data-theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login">
+            <Auth />
+          </Route>
+          <Header />
+          <Home />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
