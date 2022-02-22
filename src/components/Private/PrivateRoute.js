f26e31ecea69e92527e2934dd5894801e3a77665
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-21 16:39:25 -0800

    added my private route file

--- src/components/Private/PrivateRoute.js
+++ src/components/Private/PrivateRoute.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
 
-export default function PrivateRoute() {
-  return <div>PrivateRoute</div>;
+export default function PrivateRoute({ children, ...rest }) {
+  const { user } = useUser();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        user.user ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
 }
