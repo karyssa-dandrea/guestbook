663d94f9d143af4011f8256c71734c1e7b4a3b5d
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-21 17:31:30 -0800

    created my auth page

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { useUser } from '../../context/UserContext';
 
 export default function Auth() {
-  return <div>Auth</div>;
+  const { setUser } = useUser();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const location = useLocation();
+  const history = useHistory();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (email === process.env.AUTH_EMAIL && password === process.env.AUTH_PASSWORD) {
+      setUser(email);
+      const { from } = location.state || { from: { pathname: '/' } };
+      history.replace(from.pathname);
+    }
+  };
+
+  return (
+    <form>
+      <label>Email:</label>
+      <input id="email" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Sign In!</button>
+    </form>
+  );
 }
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Entry from '../../components/Entry/Entry';
 import Guestbook from '../../components/Guestbook/Guestbook';
+import Auth from '../Auth/Auth';
 
 export default function Home() {
   return (
     <div>
       <Guestbook />
       <Entry />
+      <Auth />
     </div>
   );
 }
